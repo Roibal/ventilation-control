eff41dd63923227d4d716dfaa8ac0a354ec2ce9e
Andreas Kleber <andreas@drosselweg7a.de>
2014-12-24 13:24:00 +0100

    adds weathermath

---
+++ weathermath.py
@@ -0,0 +1,74 @@
+#!/usr/local/bin/python
+# coding: utf-8
+
+'''
+Source
+http://www.wetterochs.de/wetter/feuchte.html
+
+Bezeichnungen:
+r = relative Luftfeuchte
+T = Temperatur in C
+TK = Temperatur in Kelvin (TK = T + 273.15)
+TD = Taupunkttemperatur in °C
+DD = Dampfdruck in hPa
+SDD = Sättigungsdampfdruck in hPa
+
+Parameter:
+a = 7.5, b = 237.3 für T >= 0
+a = 7.6, b = 240.7 für T < 0 über Wasser (Taupunkt)
+a = 9.5, b = 265.5 für T < 0 über Eis (Frostpunkt)
+
+R* = 8314.3 J/(kmol*K) (universelle Gaskonstante)
+mw = 18.016 kg/kmol (Molekulargewicht des Wasserdampfes)
+AF = absolute Feuchte in g Wasserdampf pro m3 Luft
+
+Formeln:
+
+SDD(T) = 6.1078 * 10^((a*T)/(b+T))
+
+DD(r,T) = r/100 * SDD(T)
+
+r(T,TD) = 100 * SDD(TD) / SDD(T)
+
+TD(r,T) = b*v/(a-v) mit v(r,T) = log10(DD(r,T)/6.1078)
+
+AF(r,TK) = 10^5 * mw/R* * DD(r,T)/TK; AF(TD,TK) = 10^5 * mw/R* * SDD(TD)/TK
+'''
+
+from math import pow
+import unittest
+
+R = 8314.3
+mw = 18.016
+
+def SDD(T):
+    if (T >= 0.0):
+        a = 7.5
+        b = 237.3
+    else:
+        a = 7.6
+        b = 240.7
+
+    return 6.1078 * pow( 10, ( (a*T)/(b+T) ) )
+
+def DD(r, T):
+    return (r/100.0) * SDD(T)
+
+def AF(r, T):
+    TK = T + 273.15
+    return 100000 * mw/R * DD(r, T)/TK
+
+
+# python -m unittest weathermath
+class TestWeathermath(unittest.TestCase):
+
+    def test_SDD(self):
+        self.assertAlmostEqual( SDD(20), 23.38, 2 )
+
+    def test_DD(self):
+        self.assertAlmostEqual( DD(60, 20), 14.03, 2 )
+
+    def test_AF(self):
+        self.assertAlmostEqual( AF(60, 20), 10.4, 1 )
+
+
