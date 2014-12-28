45dd447a3cb1d2560b7ec1049706f07aa545f63d
Andreas Kleber <andreas@drosselweg7a.de>
2014-12-28 13:46:41 +0100

    work on report

--- ventilation-control/report.py
+++ ventilation-control/report.py
@@ -3,6 +3,7 @@
 
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from os import curdir, sep
+import building
 
 PORT_NUMBER = 8080
 
@@ -10,7 +11,7 @@ PORT_NUMBER = 8080
 #the browser 
 class Handler(BaseHTTPRequestHandler):
 
-    def sendFile(self, mimetype, path):
+    def send_file(self, mimetype, path):
         try:
             #Open the static file requested and send it
             f = open(curdir + sep + path) 
@@ -24,23 +25,53 @@ class Handler(BaseHTTPRequestHandler):
         except IOError:
             self.send_error(404,'File Not Found: %s' % self.path)
 
+
+    def room_list(self):
+        rooms = building.getRooms()
+
+        self.send_response(200)
+        self.send_header('Content-type', "text/html")
+        self.end_headers()
+
+        # TODO refactor in page_start and page_end
+        # TODO add <html>, etc tags
+
+        self.wfile.write('<!doctype html>')
+        self.wfile.write('<title>Informant</title>')
+        self.wfile.write('<link rel=stylesheet type=text/css href="/style.css">')
+        self.wfile.write('<div class=page>')
+        self.wfile.write('<div class="top"><div class="top-title">Rooms</div></div>')
+        self.wfile.write('<ul>')
+
+        for room in rooms:
+            self.wfile.write('<li><a href=Room_%s>%s</a></li>' % (room.name, room.name))
+
+        self.wfile.write('</ul>')
+        self.wfile.write('</div>')
+
     
     # Handler for the GET requests
     def do_GET(self):
-        if self.path == "/":
-            self.path = "/index.html"
+        if self.path == '/':
+            self.path = '/index.html'
 
-        if self.path == "/index.html":
-            mimetype = "text/html"
-            self.sendFile(mimetype, self.path)
+        if self.path == '/index.html':
+            self.room_list()
         
-        elif self.path == "/style.css":
-            mimetype="text/css"
-            self.sendFile(mimetype, self.path)
+        elif self.path == '/style.css':
+            mimetype='text/css'
+            self.send_file(mimetype, self.path)
 
-        elif self.path == "/weather.json":
-            mimetype = "application/json"
+        elif self.path.startswith('/Room_'):
+            pass
             # TODO
+
+        elif self.path == '/weather.json':
+            mimetype = 'application/json'
+            # TODO
+
+        else:
+            self.send_error(404,'Not Found: %s' % self.path)
 
 
 try:
