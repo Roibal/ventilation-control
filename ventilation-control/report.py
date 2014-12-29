5764f0de4ee9e93f0004f4dcb14a688568d16e0a
Andreas Kleber <andreas@drosselweg7a.de>
2014-12-29 20:29:25 +0100

    adds demo graph to room

--- ventilation-control/report.py
+++ ventilation-control/report.py
@@ -26,28 +26,80 @@ class Handler(BaseHTTPRequestHandler):
             self.send_error(404,'File Not Found: %s' % self.path)
 
 
-    def room_list(self):
+    def print_page_header(self, title):
+        self.wfile.write('<!doctype html>\n')
+        self.wfile.write('<html>\n')
+        self.wfile.write('<head>\n')
+        self.wfile.write('<title>%s</title>\n' % title)
+        self.wfile.write('<link rel=stylesheet type=text/css href="/style.css">\n')
+        self.wfile.write('</head>\n')
+        self.wfile.write('<body>\n')
+        self.wfile.write('<div class=page>\n')
+        self.wfile.write('<div class="top"><div class="top-title">%s</div></div>\n' % title)
+
+    def print_room_footer(self):
+         self.wfile.write('</div>\n')
+         self.wfile.write('</body>\n')
+         self.wfile.write('</html>\n')
+
+
+    def display_room_list(self):
         rooms = building.getRooms()
 
         self.send_response(200)
         self.send_header('Content-type', "text/html")
         self.end_headers()
 
-        # TODO refactor in page_start and page_end
-        # TODO add <html>, etc tags
+        self.print_page_header('Rooms')
 
-        self.wfile.write('<!doctype html>')
-        self.wfile.write('<title>Informant</title>')
-        self.wfile.write('<link rel=stylesheet type=text/css href="/style.css">')
-        self.wfile.write('<div class=page>')
-        self.wfile.write('<div class="top"><div class="top-title">Rooms</div></div>')
         self.wfile.write('<ul>')
 
         for room in rooms:
-            self.wfile.write('<li><a href=Room_%s>%s</a></li>' % (room.name, room.name))
+            self.wfile.write('<li><a href=Room_%s>%s</a></li>\n' % (room.name, room.name))
 
         self.wfile.write('</ul>')
-        self.wfile.write('</div>')
+
+        self.print_room_footer()
+       
+
+    def display_room(self, roomName):
+        self.print_page_header(roomName)
+        self.wfile.write('<div id="container" style="width:100%; height:400px;"></div>\n')
+        self.wfile.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>\n')
+        self.wfile.write('<script src="http://code.highcharts.com/stock/highstock.js"></script>\n')
+
+        s = """
+<script type="text/javascript">
+$(function () {
+
+    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+        // Create the chart
+        $('#container').highcharts('StockChart', {
+
+
+            rangeSelector : {
+                selected : 1
+            },
+
+            title : {
+                text : 'AAPL Stock Price'
+            },
+
+            series : [{
+                name : 'AAPL',
+                data : data,
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }]
+        });
+    });
+
+});
+</script>
+"""
+        self.wfile.write(s)
+        self.print_room_footer()
 
     
     # Handler for the GET requests
@@ -56,15 +108,15 @@ class Handler(BaseHTTPRequestHandler):
             self.path = '/index.html'
 
         if self.path == '/index.html':
-            self.room_list()
+            self.display_room_list()
         
         elif self.path == '/style.css':
             mimetype='text/css'
             self.send_file(mimetype, self.path)
 
         elif self.path.startswith('/Room_'):
-            pass
-            # TODO
+            roomName = self.path[6:]
+            self.display_room(roomName)
 
         elif self.path == '/weather.json':
             mimetype = 'application/json'
