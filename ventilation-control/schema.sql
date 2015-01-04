3b5f69b25d4b746f117d156a3c62b6ffdce169d4
Andreas Kleber <andreas@drosselweg7a.de>
2015-01-04 19:30:38 +0100

    adds index for weather, adds table for actor status

--- ventilation-control/schema.sql
+++ ventilation-control/schema.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS weather;
-CREATE TABLE weather (
+DROP TABLE IF EXISTS 'weather';
+CREATE TABLE 'weather' (
   'date' timestamp not null,
   'room' text not null,
   'inside_temperature' REAL not null,
@@ -9,6 +9,14 @@ CREATE TABLE weather (
   'outside_temperature' REAL not null,
   'outside_humidity' REAL not null,
   'ventilation_recommended' BOOLEAN DEFAULT 0
+);
+
+CREATE INDEX 'weather_index' ON 'weather' ('date' DESC, 'room');
+
+DROP TABLE IF EXISTS 'actor_status';
+CREATE TABLE 'actor_status' (
+  'room' text not null,
+  'powered_on' timestamp not null
 );
 
 COMMIT;
